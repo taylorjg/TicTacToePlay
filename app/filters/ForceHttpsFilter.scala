842032f8916d380693ade96a06a5e15f7df1f05c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-09-04 11:13:38 +0100

    Tweak to ForceHttpsFilter

--- app/filters/ForceHttpsFilter.scala
+++ app/filters/ForceHttpsFilter.scala
@@ -1,9 +1,8 @@
 package filters
 
-import akka.stream.Materializer
 import javax.inject._
 
-import play.api.Logger
+import akka.stream.Materializer
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -16,12 +15,17 @@ class ForceHttpsFilter @Inject()(implicit override val mat: Materializer,
 
   override def apply(nextFilter: RequestHeader => Future[Result])
                     (requestHeader: RequestHeader): Future[Result] = {
-    Logger.info("Checking for x-forwarded-proto header")
     requestHeader.headers.get("x-forwarded-proto") match {
-      case Some(header) if (header != "https") => {
-        val secureUrl = s"https://${requestHeader.host}${requestHeader.uri}"
-        Logger.info(s"secureUrl: $secureUrl")
-        Future.successful(Results.MovedPermanently(secureUrl))
+      case Some(header) => {
+        if (header == "https") {
+          nextFilter(requestHeader) map { result =>
+            result.withHeaders("Strict-Transport-Security" -> "max-age=31536000")
+          }
+        }
+        else {
+          val secureUrl = s"https://${requestHeader.host}${requestHeader.uri}"
+          Future.successful(Results.MovedPermanently(secureUrl))
+        }
       }
       case _ => nextFilter(requestHeader)
     }
