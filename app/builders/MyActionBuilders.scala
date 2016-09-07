c48f9ec7c64dd458d38369963df08240ed5c373c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-09-07 21:09:48 +0100

    Added another action builder to support optional authentication

--- app/builders/MyActionBuilders.scala
+++ app/builders/MyActionBuilders.scala
@@ -29,9 +29,34 @@ trait MyActionBuilders{
             case LookupUsernameResponse(Some(user)) =>
               val authenticatedRequest = new AuthenticatedRequest(user, request)
               block(authenticatedRequest)
-            case LookupUsernameResponse(None) => Future.successful(Redirect(indexCall).withNewSession)
+            case LookupUsernameResponse(None) =>
+              Future.successful(Redirect(indexCall).withNewSession)
           }
         case None => Future.successful(Redirect(indexCall).withNewSession)
+      }
+    }
+  }
+
+  def OptionallyAuthenticatedBuilder = new ActionBuilder[({ type R[A] = AuthenticatedRequest[A, Option[User]] })#R] with Results {
+
+    import actors.UsersActor.{LookupUsernameRequest, LookupUsernameResponse}
+
+    private val indexCall = routes.TicTacToeController.index()
+
+    override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A, Option[User]]) => Future[Result]): Future[Result] = {
+      request.session.get("username") match {
+        case Some(username) =>
+          val response = (mainActor ? LookupUsernameRequest(username)).mapTo[LookupUsernameResponse]
+          response flatMap {
+            case LookupUsernameResponse(Some(user)) =>
+              val authenticatedRequest = new AuthenticatedRequest(Option[User](user), request)
+              block(authenticatedRequest)
+            case LookupUsernameResponse(None) =>
+              Future.successful(Redirect(indexCall).withNewSession)
+          }
+        case None =>
+          val authenticatedRequest = new AuthenticatedRequest(Option.empty[User], request)
+          block(authenticatedRequest)
       }
     }
   }
