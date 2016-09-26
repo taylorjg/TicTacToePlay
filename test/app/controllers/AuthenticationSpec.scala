1980bc4c514b5a9b5b3500d5a31c4c66c11f420d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-09-26 23:25:59 +0100

    Added a unit test re accessing the registeredGame page whilst logged in (more of an integration test actually).

--- test/app/controllers/AuthenticationSpec.scala
+++ test/app/controllers/AuthenticationSpec.scala
@@ -2,9 +2,10 @@ package app.controllers
 
 import controllers.{TicTacToeController, routes}
 import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
-import play.api.mvc.Results
+import play.api.mvc.{Results, Security}
 import play.api.test.Helpers._
 import play.api.test._
+import controllers.AuthenticationController._
 
 class AuthenticationSpec
   extends PlaySpec
@@ -14,11 +15,32 @@ class AuthenticationSpec
   private val landingPageUrl = routes.TicTacToeController.index().url
 
   "TicTacToeController#registeredGame" when {
+
     "not logged in" should {
       "redirect to the landing page" in {
         val controller = app.injector.instanceOf[TicTacToeController]
         val resultFuture = controller.registeredGame().apply(FakeRequest())
         redirectLocation(resultFuture) must be(Some(landingPageUrl))
+      }
+    }
+
+    "logged in" should {
+      "not redirect to the landing page" in {
+
+        val username = "username1"
+        val password = "password1"
+
+        val authenticationController = app.injector.instanceOf[controllers.AuthenticationController]
+        val registerResultFuture = authenticationController.register().apply(FakeRequest().withFormUrlEncodedBody(
+          USERNAME_FIELD -> username,
+          PASSWORD_FIELD -> password,
+          PASSWORD2_FIELD -> password))
+        await(registerResultFuture)
+
+        val controller = app.injector.instanceOf[TicTacToeController]
+        val fakeRequest = FakeRequest().withSession(Security.username -> username)
+        val resultFuture = controller.registeredGame().apply(fakeRequest)
+        status(resultFuture) must be(OK)
       }
     }
   }
