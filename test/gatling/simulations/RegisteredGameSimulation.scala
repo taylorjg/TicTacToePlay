30cadf33ce0f9c60340ca94dfaa1761ff736ebcf
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-02 23:04:15 +0100

    Reformatted Gatling scripts

--- test/gatling/simulations/RegisteredGameSimulation.scala
+++ test/gatling/simulations/RegisteredGameSimulation.scala
@@ -3,25 +3,25 @@ import io.gatling.http.Predef._
 
 class RegisteredGameSimulation extends Simulation {
 
-	val httpProtocol = http
-		.baseURL("http://localhost:9000")
+  val httpProtocol = http
+    .baseURL("http://localhost:9000")
 
-	val getHeaders = Map(
+  val getHeaders = Map(
     "Cookie" -> "PLAY_SESSION=4c1c96a40c99ab29ec1b274fcfc54bba6718c4f1-username=Jon")
 
-	val postHeaders = Map(
-		"Cookie" -> "PLAY_SESSION=4c1c96a40c99ab29ec1b274fcfc54bba6718c4f1-username=Jon",
-		"Content-Type" -> "application/json")
+  val postHeaders = Map(
+    "Cookie" -> "PLAY_SESSION=4c1c96a40c99ab29ec1b274fcfc54bba6718c4f1-username=Jon",
+    "Content-Type" -> "application/json")
 
-	val scn = scenario("RegisteredGameSimulation")
-		.exec(http("loadPage")
-			.get("/registeredGame")
-			.headers(getHeaders))
-		.pause(1)
-		.exec(http("humanMove1")
-			.post("/api/computerMove")
-			.headers(postHeaders)
-			.body(StringBody("""{"board":"------X--","player1Piece":"X","player2Piece":"O"}"""))
+  val scn = scenario("RegisteredGameSimulation")
+    .exec(http("loadPage")
+      .get("/registeredGame")
+      .headers(getHeaders))
+    .pause(1)
+    .exec(http("humanMove1")
+      .post("/api/computerMove")
+      .headers(postHeaders)
+      .body(StringBody("""{"board":"------X--","player1Piece":"X","player2Piece":"O"}"""))
       .check(jsonPath("$.board").saveAs("board")))
     .exec(session => {
       println(s"session: $session")
@@ -48,5 +48,5 @@ class RegisteredGameSimulation extends Simulation {
       session
     })
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
--- test/gatling/simulations/UnregisteredGameSimulation.scala
+++ test/gatling/simulations/UnregisteredGameSimulation.scala
@@ -3,26 +3,26 @@ import io.gatling.http.Predef._
 
 class UnregisteredGameSimulation extends Simulation {
 
-	val httpProtocol = http
-		.baseURL("http://localhost:9000")
-		.inferHtmlResources()
+  val httpProtocol = http
+    .baseURL("http://localhost:9000")
+    .inferHtmlResources()
 
-	val postHeaders = Map("Content-Type" -> "application/json")
+  val postHeaders = Map("Content-Type" -> "application/json")
 
-	val scn = scenario("UnregisteredGameSimulation")
-		.exec(http("loadPage")
-			.get("/unregisteredGame")
+  val scn = scenario("UnregisteredGameSimulation")
+    .exec(http("loadPage")
+      .get("/unregisteredGame")
     )
-		.pause(1)
-		.exec(http("humanMove1")
-			.post("/api/computerMove")
-			.headers(postHeaders)
-			.body(StringBody("""{"board":"X--------","player1Piece":"X","player2Piece":"O"}"""))
-			.check(jsonPath("$.board").saveAs("board")))
-	  .exec(session => {
-			println(s"session: $session")
-			session
-		})
+    .pause(1)
+    .exec(http("humanMove1")
+      .post("/api/computerMove")
+      .headers(postHeaders)
+      .body(StringBody("""{"board":"X--------","player1Piece":"X","player2Piece":"O"}"""))
+      .check(jsonPath("$.board").saveAs("board")))
+    .exec(session => {
+      println(s"session: $session")
+      session
+    })
     .pause(1)
     .exec(http("humanMove2")
       .post("/api/computerMove")
@@ -44,5 +44,5 @@ class UnregisteredGameSimulation extends Simulation {
       session
     })
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
