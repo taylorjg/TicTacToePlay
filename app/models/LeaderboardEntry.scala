fcb6b06469ea062155a31c3f87f2b55f431b8e71
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-09 13:43:04 +0100

    LeaderboardEntry is now just a case class with no extra stuff

--- app/models/LeaderboardEntry.scala
+++ app/models/LeaderboardEntry.scala
@@ -1,19 +1,19 @@
 package models
 
-case class LeaderboardEntry(username: String, numWon: Int, numDrawn: Int, numLost: Int) extends Ordered[LeaderboardEntry] {
+case class LeaderboardEntry(username: String, numWon: Int, numDrawn: Int, numLost: Int)
 
-  override def compare(that: LeaderboardEntry): Int = {
-    val thisScore = this.points
-    val thatScore = that.points
-    val comparison1 = -(thisScore compare thatScore)
-    val comparison2 = username compare that.username
-    if (comparison1 != 0) comparison1 else comparison2
+object LeaderboardEntry {
+
+  implicit def ordering: Ordering[LeaderboardEntry] = new Ordering[LeaderboardEntry] {
+    override def compare(x: LeaderboardEntry, y: LeaderboardEntry): Int = {
+      val comparison1 = -(x.points compare y.points)
+      val comparison2 = x.username compare y.username
+      if (comparison1 != 0) comparison1 else comparison2
+    }
   }
 
-  val played: Int = calcPlayed(this)
-  val points: Int = calcPoints(this)
-
-  private def calcPlayed(le: LeaderboardEntry): Int = le.numWon + le.numDrawn + le.numLost
-
-  private def calcPoints(le: LeaderboardEntry): Int = le.numWon * 3 + le.numDrawn
+  implicit class LeaderboardEntryExtensions(le: LeaderboardEntry) {
+    def played: Int = le.numWon + le.numDrawn + le.numLost
+    def points: Int = le.numWon * 3 + le.numDrawn
+  }
 }
