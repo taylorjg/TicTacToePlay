99189c4bc5f78c3dafa15ecdddd4dd84afebed5f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-13 23:23:49 +0100

    Cleaned up userDisplayName method

--- app/utils/Utils.scala
+++ app/utils/Utils.scala
@@ -4,15 +4,15 @@ import play.twirl.api.{Html, HtmlFormat}
 
 object Utils {
 
-  val MAX_USER_DISPLAY_LENGTH = 15
+  final val MAX_USER_DISPLAY_LENGTH = 15
 
   def userDisplayName(username: String): Html = {
-    if (username.length <= MAX_USER_DISPLAY_LENGTH) HtmlFormat.escape(username)
+    if (username.length <= MAX_USER_DISPLAY_LENGTH)
+      HtmlFormat.escape(username)
     else {
-      val bit1 = HtmlFormat.escape(username.substring(0, MAX_USER_DISPLAY_LENGTH))
-      val bit2 = HtmlFormat.raw("&hellip;")
-      val bits = scala.collection.immutable.Seq(bit1, bit2)
-      HtmlFormat.fill(bits)
+      val croppedUsername = HtmlFormat escape (username take MAX_USER_DISPLAY_LENGTH)
+      val ellipsis = HtmlFormat raw "&hellip;"
+      HtmlFormat.fill(List(croppedUsername, ellipsis))
     }
   }
 }
