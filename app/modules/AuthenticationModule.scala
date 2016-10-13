81592460811ceee09a605ceefc29bc04fbb61d57
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-13 23:09:26 +0100

    Another tweak to UserServiceImpl

--- app/modules/AuthenticationModule.scala
+++ app/modules/AuthenticationModule.scala
@@ -23,19 +23,13 @@ class UserServiceImpl @Inject()(@Named("mainActor") val mainActor: ActorRef) ext
   import scala.concurrent.ExecutionContext.Implicits.global
 
   override def lookupUsername(username: String): Future[Option[User]] =
-    (mainActor ? LookupUsernameRequest(username)).mapTo[LookupUsernameResponse] map {
-      response => response.user
-    }
+    (mainActor ? LookupUsernameRequest(username)).mapTo[LookupUsernameResponse] map (_.user)
 
   override def login(username: String, password: String): Future[Option[User]] =
-    (mainActor ? LoginRequest(username, password)).mapTo[LoginResponse] map {
-      response => response.user
-    }
+    (mainActor ? LoginRequest(username, password)).mapTo[LoginResponse] map (_.user)
 
   override def registerUser(username: String, password: String): Future[Option[User]] =
-    (mainActor ? RegisterUserRequest(username, password)).mapTo[RegisterUserResponse] map {
-      response => response.user
-    }
+    (mainActor ? RegisterUserRequest(username, password)).mapTo[RegisterUserResponse] map (_.user)
 }
 
 class AuthenticationModule extends AbstractModule with AkkaGuiceSupport {
