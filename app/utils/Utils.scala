6707f6f3d9730cb978ebcc99602b256e96bf145d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-13 23:27:25 +0100

    Another tweak to userDisplayName method

--- app/utils/Utils.scala
+++ app/utils/Utils.scala
@@ -6,13 +6,13 @@ object Utils {
 
   final val MAX_USER_DISPLAY_LENGTH = 15
 
-  def userDisplayName(username: String): Html = {
-    if (username.length <= MAX_USER_DISPLAY_LENGTH)
-      HtmlFormat.escape(username)
-    else {
-      val croppedUsername = HtmlFormat escape (username take MAX_USER_DISPLAY_LENGTH)
-      val ellipsis = HtmlFormat raw "&hellip;"
-      HtmlFormat.fill(List(croppedUsername, ellipsis))
+  def userDisplayName(username: String): Html =
+    username.length match {
+      case length if length <= MAX_USER_DISPLAY_LENGTH =>
+        HtmlFormat escape username
+      case _ =>
+        val croppedUsername = HtmlFormat escape (username take MAX_USER_DISPLAY_LENGTH)
+        val ellipsis = HtmlFormat raw "&hellip;"
+        HtmlFormat.fill(List(croppedUsername, ellipsis))
     }
-  }
 }
