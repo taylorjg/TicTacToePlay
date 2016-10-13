00c4b37d6d7a923ea673c97bc1fc03b7c94c831c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-10-13 23:58:35 +0100

    Don't need override on methods in UserServiceImpl

--- app/services/UserServiceImpl.scala
+++ app/services/UserServiceImpl.scala
@@ -14,12 +14,12 @@ class UserServiceImpl @Inject()(@Named("mainActor") mainActor: ActorRef) extends
   import defaults.Defaults.ASK_TIMEOUT
   import models.User
 
-  override def lookupUsername(username: String): Future[Option[User]] =
+  def lookupUsername(username: String): Future[Option[User]] =
     (mainActor ? LookupUsernameRequest(username)).mapTo[LookupUsernameResponse] map (_.user)
 
-  override def login(username: String, password: String): Future[Option[User]] =
+  def login(username: String, password: String): Future[Option[User]] =
     (mainActor ? LoginRequest(username, password)).mapTo[LoginResponse] map (_.user)
 
-  override def registerUser(username: String, password: String): Future[Option[User]] =
+  def registerUser(username: String, password: String): Future[Option[User]] =
     (mainActor ? RegisterUserRequest(username, password)).mapTo[RegisterUserResponse] map (_.user)
 }
